a81c81df0d0987a567bb1e4036a98c51ead5a220
Ynfuien <ynfuien@gmail.com>
2023-08-16 22:46:30 +0200

    Added game preview in readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Snake in C#
 Simple snake game, done just for fun.
+
+# Media
+![Gameplay](https://i.imgur.com/QwwVdCb.gif)
+
+# License
+This project uses [GNU GPLv3](https://github.com/Ynfuien/snake-csharp/blob/main/LICENSE) license.
--- Snake/Properties/AssemblyInfo.cs
+++ Snake/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Snake")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("YApps")]
 [assembly: AssemblyProduct("Snake")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
